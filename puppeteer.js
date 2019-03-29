cec3e194d8934461f21c5f4aa3998101dfad40f3
Rana <rana@p-united.com>
2019-03-29 10:31:32 +0700

    change screenshot process

--- puppeteer.js
+++ puppeteer.js
@@ -16,11 +16,16 @@ module.exports.screenshot =  (body) =>{
             let browser = await puppeteer.launch({executablePath: process.env.EXECUTABLE_PATH});
             let page = await browser.newPage();
             const override = Object.assign(page.viewport(), {width: width});
+            await page.setViewport(override);
             if(isAuthenticated){
                 await page.authenticate({username:body.authentication.username, password:body.authentication.password});
             }
             await page.goto(url, {waitUntil: 'load',timeout: 0});
-            await page.setViewport(override);
+            await page.evaluate(_ => {
+                while (document.body.scrollHeight < (window.scrollY + window.innerHeight)) {
+                  window.scrollBy(0, window.innerHeight);
+                }
+              });
             await page.screenshot({ path: `./${fileName}`,fullPage: true });
             await page.close();
             await browser.close();
