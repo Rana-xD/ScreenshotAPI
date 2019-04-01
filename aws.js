0b74669d6f6d68d069a095595756ce499e5ea98a
Vinei <ro.vinei@yahoo.com>
2019-04-01 17:07:50 +0700

    Reflect Pupetteer To Login Before Capture

--- aws.js
+++ aws.js
@@ -10,36 +10,33 @@ dotenv.config();
 
 
 module.exports.uploadImage = (file,path) => {
-	    return new Promise((resolve,reject)=>{
-	        fs.readFile(file, function (err, data) {
-	            if (err) throw err; // Something went wrong!
-	                var fileName = file.substring(5);
-	                var params = {
-	                    Bucket:process.env.BUCKET,
-	                    Key: path + fileName, //file.name doesn't exist as a property
-	                    Body: data,
-                        ACL: 'public-read',
-                        ContentType: 'image/png'
-	                };
-	                s3.upload(params, function (err, data) {
-	                    // Whether there is an error or not, delete the temp file
-	                    if (err) {
-	                        console.log('ERROR MSG: ', err);
-	                        reject(err);
-	                    } else {
-	                        fs.unlink(file, function (err) {
-	                            if (err) {
-									console.log(err);
-									reject(err);
-								}
-								resolve(data.Location);
-	                        });
-	                        
-	                    }
-	                });
-	            });
-	
-	    });
-	
-	
-	}
+	return new Promise((resolve,reject)=>{
+		fs.readFile(file, function (err, data) {
+			if (err) throw err; // Something went wrong!
+				var fileName = file.substring(5);
+				var params = {
+					Bucket:process.env.BUCKET,
+					Key: path + fileName, //file.name doesn't exist as a property
+					Body: data,
+					ACL: 'public-read',
+					ContentType: 'image/png'
+				};
+				s3.upload(params, function (err, data) {
+					// Whether there is an error or not, delete the temp file
+					if (err) {
+						console.log('ERROR MSG: ', err);
+						reject(err);
+					} else {
+						fs.unlink(file, function (err) {
+						if (err) {
+							console.log(err);
+							reject(err);
+						}
+					resolve(data.Location);
+				});
+						
+				}
+			});
+		});
+	});
+}
