6d3fd6f05872205896c6f56b0021912aea5d8ed4
Vinei <vineiisgood@gmail.com>
2019-09-12 10:01:51 +0700

    BC013_34, Wait For a Specific Amount of Time Before Capture the Page

--- puppeteer.js
+++ puppeteer.js
@@ -2,23 +2,23 @@ const puppeteer = require('puppeteer-core');
 const randomstring = require('randomstring');
 
 
-module.exports.screenshot =  (body) =>{
+module.exports.screenshot = (body) => {
 	let url = body.url;
 	let width = body.width;
 	let isAuthenticated = body.authentication ? true : false;
 	let hasLoginPage = body.login_bypass ? true : false;
-	
+	let waitTime = body.wait_time || 0;
+
 	let fileName = body.fileName || `${randomstring.generate({charset: 'hex'})}.png`;
-	
+
 	return new Promise(async (resolve, reject) => {
-		try{
-			console.log(body);
+		try {
 			let defaultHeight = await getHeight(width);
 			let browser = await puppeteer.launch({
 				executablePath: process.env.EXECUTABLE_PATH,
 				defaultViewport: {
-					width:width,
-					height:defaultHeight
+					width: width,
+					height: defaultHeight
 				}
 			});
 			let page = await browser.newPage();
@@ -26,15 +26,15 @@ module.exports.screenshot =  (body) =>{
 			// 	width: width
 			// });
 			// await page.setViewport(override);
-			if(isAuthenticated){
+			if (isAuthenticated) {
 				await page.authenticate({
-					username: body.authentication.username, 
+					username: body.authentication.username,
 					password: body.authentication.password
 				});
 			}
-			
+
 			// Bypass login form
-			if(hasLoginPage) {
+			if (hasLoginPage) {
 				let loginPageUrl = body.login_bypass.login_page_url.trim(),
 					loginId = body.login_bypass.login_id_value.trim(),
 					loginPassword = body.login_bypass.login_pass_value,
@@ -55,8 +55,8 @@ module.exports.screenshot =  (body) =>{
 					waitUntil: 'load',
 					timeout: 0
 				});
-				
-				
+
+
 				// Wait for page to loaded completely
 				// await page.waitForFunction("document.readyState == 'completed'", {
 				// 	polling: 100, // 0.1 second
@@ -69,12 +69,18 @@ module.exports.screenshot =  (body) =>{
 			});
 			const height = await page.evaluate(_ => {
 				return document.body.scrollHeight;
-			  });
+			});
 			// await page.evaluate(_ => {
 			// 	while (document.body.scrollHeight < (window.scrollY + window.innerHeight)) {
 			// 	  window.scrollBy(0, window.innerHeight);
 			// 	}
 			// });
+
+			// Wait for a specific period before capture
+			await new Promise((resolve, reject) => {
+				setTimeout(resolve, (parseInt(waitTime) * 10000));
+			});
+
 			await page.screenshot({
 				path: `./${fileName}`,
 				fullPage: true
@@ -86,20 +92,19 @@ module.exports.screenshot =  (body) =>{
 			await page.close();
 			await browser.close();
 			resolve(fileName);
-		}catch(e){
+		} catch (e) {
 			console.log(e);
 			reject(e)
 		}
 	});
 }
 
-function getHeight(width){
+function getHeight(width) {
 	return new Promise(async (resolve, reject) => {
-	if(width<500){
-	  resolve(600);
-	}
-	else{
-	  resolve(height = Math.round((width * 9) / 16));
-	}
-});
+		if (width < 500) {
+			resolve(600);
+		} else {
+			resolve(height = Math.round((width * 9) / 16));
+		}
+	});
 }
