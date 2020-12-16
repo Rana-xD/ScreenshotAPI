365bdf43b7d95a40554b43bc5bff4f8dbfe6e374
Rana <rana@p-united.com>
2020-12-16 17:22:29 +0700

    Whitelisting IP

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const ipfilter = require('express-ipfilter').IpFilter
+const IpDeniedError = require('express-ipfilter').IpDeniedError
 const app = express();
 dotenv.config();
+
+
+
+const ips = ['160.86.236.137','18.180.24.82','3.115.214.232','18.138.255.31'];
+app.use(ipfilter(ips, { mode: 'allow' }));
+
+if (app.get('env') === 'development') {
+	app.use((err, req, res, _next) => {
+	  console.log('Error handler', err)
+	  if (err instanceof IpDeniedError) {
+		res.status(403)
+	  } else {
+		res.status(err.status || 500)
+	  }
+  
+	  res.send({
+		message: 'You are forbidden',
+		error: err
+	  })
+	})
+  }
+  
+  // production error handler
+  // no stacktraces leaked to user
+app.use((err, req, res, _next) => {
+    console.log('Error handler', err)
+    res.status(err.status || 500)
+    res.send({
+      message: err.message,
+      error: {}
+    })
+})
+
 
 module.exports = app;
 
