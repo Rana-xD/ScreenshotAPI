be24c779767ba59a4e04c6aedb1a76ad66623776
Vinei <vineiisgood@gmail.com>
2021-02-01 16:50:14 +0700

    Set PM2 configuration for max_old_space_size to 1GB

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,6 +15,9 @@ module.exports = {
       },
       env_production: {
          NODE_ENV: 'production'
-      }
+      },
+      node_args: [
+         "--max_old_space_size=1024"
+      ]
    }]
 };
