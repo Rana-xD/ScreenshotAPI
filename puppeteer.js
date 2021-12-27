3f1d405a3133b535b650cfa791168f4f549e26c8
Rana <rana@p-united.com>
2021-12-27 10:39:15 +0700

    switch to use chrome

--- puppeteer.js
+++ puppeteer.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer-firefox');
+const puppeteer = require('puppeteer');
 const randomstring = require('randomstring');
 const fs = require('fs');
 // const logger = require('./log')
